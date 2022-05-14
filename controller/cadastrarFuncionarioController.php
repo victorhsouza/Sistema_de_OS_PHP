31441e1fc85fe2fc17f4f2be53df459e2f422cec
victorhsouza <victorhsouzaa@gmail.com>
2022-05-14 12:35:53 -0300

    Cadastrando funcionario mas ainda sem juntar com o id do ususario

--- controller/cadastrarFuncionarioController.php
+++ controller/cadastrarFuncionarioController.php
@@ -1,6 +1,8 @@
 <?php 
 require_once '../dao/funcionarioDAO.php';
 require_once '../dto/funcionarioDTO.php';
+require_once '../dao/conexao/conexao.php';
+
 
 $nome = $_POST['nome'];
 $cpf = $_POST['cpf'];
@@ -10,6 +12,10 @@ $senha = $_POST['senha'];
 $perfil = $_POST['perfil'];
 $sexo = $_POST['sexo'];
 $datanasc = $_POST['data'];
+
+echo "$usuario <br>"; 
+echo "$senha <br>";
+echo "$perfil <br>";
 
 
 $funcionarioDTO = new funcionarioDTO();
@@ -23,14 +29,23 @@ $sexo = $funcionarioDTO->setSexo($sexo);
 $datanasc = $funcionarioDTO->setDatanasc($datanasc);
 
 $funcionarioDAO = new funcionarioDAO();
-$funcionarioDAO->salvar($funcionarioDTO);
-//$funcionarioDAO->setUsuario($usuario,$senha,$perfil);
-
-        echo "
+$salvar = $funcionarioDAO->salvar($funcionarioDTO);
+$user = $funcionarioDAO->setUsuario($funcionarioDTO);
+        if($salvar && $user){
+            echo "
             <script>
                 alert('Funcionario cadastrado com sucesso')
                 window.location = '../view/listarAllFuncionario.php
             </script>    
         ";
+        }else{
+            echo "Deu bom nao";
+            $bd = new Conexao();
+            $conexao = $bd->getInstance();
+            if(!isset($conexao->connect_errno)){
+                return $conexao->error;
+            }
+        }
+        
         
         
--- dao/funcionarioDAO.php
+++ dao/funcionarioDAO.php
@@ -6,11 +6,29 @@ class funcionarioDAO{
 
     public function salvar(funcionarioDTO $funcionarioDTO){
 
+     
         $nome = $funcionarioDTO->getNome();
         $cpf = $funcionarioDTO->getCpf();
         $cargo = $funcionarioDTO->getCargo();
         $sexo = $funcionarioDTO->getSexo();
         $datanasc = $funcionarioDTO->getDatanasc();
+        
+        $bd = new Conexao();
+        $conexao =  $bd->getInstance();
+
+        //$idusuario = $conexao->insert_id;
+
+        $sql = $conexao->query("insert into funcionario value ('$cpf','$nome','$cargo','$sexo','$datanasc', default)");
+        return $sql;
+
+        if(!$sql){
+            $mg =  $conexao->connect_error;
+            echo $mg;
+        }
+        
+    }
+
+    function setUsuario(funcionarioDTO $funcionarioDTO){
         $usuario = $funcionarioDTO->getUsuario();
         $senha = $funcionarioDTO->getSenha();
         $perfil = $funcionarioDTO->getPerfil();
@@ -20,37 +38,16 @@ class funcionarioDAO{
         $bd = new Conexao();
         $conexao =  $bd->getInstance();
 
-        $stmt = $conexao->prepare("insert into usuario values(?, ?, ?)");
+        $ex = $conexao->query("insert into usuario values(default,'$usuario','$senha',$perfil)");
+        return $ex;
+
+
+        /*$stmt = $conexao->prepare("insert into usuario values (?, ?, ?)");
         $stmt->bind_param('ssi',$user,$pass,$perfil_idperfil);
         $user = $usuario;
         $pass = $senha;
         $perfil_idperfil = $perfil;
-        $stmt->execute();
-
-        $idusuario = $conexao->insert_id;
-
-        $sql = $conexao->query("insert into funcionario value ('$cpf','$nome','$cargo','$sexo','$datanasc', $idusuario)");
-        return $sql;
-
-        if(!$sql){
-            $mg =  $conexao->error;
-            echo $mg;
-        }
-        
-    }
-
-    function setUsuario($usuario,$senha,$perfil){
-        /*$usuario = $funcionarioDTO->getUsuario();
-        $senha = $funcionarioDTO->getSenha();
-        $perfil = $funcionarioDTO->getPerfil();*/
-        $bd = new Conexao();
-        $conexao =  $bd->getInstance();
-        $sql = $conexao->query("insert into usuario values('$usuario', '$senha', $perfil)");
-        return $sql;
-        if(!$sql){
-            $mg =  $conexao->error;
-            echo $mg;
-        }
+        return $stmt->execute();*/
 
     }
 
@@ -65,7 +62,7 @@ class funcionarioDAO{
     function getFuncionarioById($cpf){
         $bd = new Conexao();
         $conexao =  $bd->getInstance();
-        $sql = $conexao->query(" select * from funcionario where cpf = '$cpf'");
+        $sql = $conexao->query("select * from funcionario where cpf = '$cpf'");
         $assoc = $sql->fetch_assoc();
 
         return $assoc;
